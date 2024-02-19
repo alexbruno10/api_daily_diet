948cd8b48e9dbc57f26e802f8f4e6448370a8f63
Alex Bruno Neri Humberto <alex_brunoneri@hotmail.com>
2024-02-19 20:21:13 -0300

    fix: add column session id in table meals

--- db/migrations/20240219224955_create_meals.ts
+++ db/migrations/20240219224955_create_meals.ts
@@ -3,6 +3,7 @@ import type { Knex } from 'knex'
 export async function up(knex: Knex): Promise<void> {
   await knex.schema.createTable('meals', (table) => {
     table.uuid('id').primary()
+    table.uuid('session_id').index()
     table.text('name').notNullable()
     table.text('description').notNullable()
     table.boolean('isOnTheDiet').defaultTo(1)
