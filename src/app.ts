c260c585f2573ea59856681130e7213af468ffbe
Alex Bruno Neri Humberto <alex_brunoneri@hotmail.com>
2024-02-19 20:20:22 -0300

    feat: create middleware to verify exist session id

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,10 @@
 import fastify from 'fastify'
 import { mealsTransactions } from './routes/meals'
+import cookie from '@fastify/cookie'
 
 export const app = fastify()
+
+app.register(cookie)
 
 app.register(mealsTransactions, {
   prefix: 'meals',
