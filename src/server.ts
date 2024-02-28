1507d906091c4af40e869325506dd3d411d099d7
Alex Bruno Neri Humberto <alex_brunoneri@hotmail.com>
2024-02-28 20:00:40 -0300

    fix: bind host 0.0.0.0 for docker

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,18 @@
 import { app } from './app'
 import { env } from './env'
 
-app
-  .listen({
-    port: env.PORT,
-  })
-  .then(() => {
-    console.log('Server up!')
-  })
+// app
+//   .listen({
+//     port: env.PORT,
+//   })
+//   .then(() => {
+//     console.log(`Server up na porta ${env.PORT}`)
+//   })
+
+app.listen({ port: env.PORT, host: '0.0.0.0' }, (err) => {
+  if (err) {
+    app.log.error(err)
+    process.exit(1)
+  }
+  console.log('Server up!')
+})
